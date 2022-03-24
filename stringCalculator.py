eef110938e7957a3e97cf59c209214596c2214a8
siva-rama-pandiyan <sivaamc15@gmail.com>
2022-03-25 02:25:29 +0530

    modified method to handle two and unknown amount of numbers

--- stringCalculator.py
+++ stringCalculator.py
@@ -7,6 +7,14 @@ def test():
     assert(add("4") == 4)
     assert(add("2") == 2)
 
+    #Two Number
+    assert(add("1,2") == 3)
+    assert(add("4,5") == 9)
+
+    #multiple numbers
+    assert(add("1,2,3,4,5") == 15)
+    assert(add("20,30,40,50") == 140)
+
     print("All Test cases Completed Successfully")
 
 #Main function
@@ -15,5 +23,12 @@ def add(inputString):
         return 0
     elif len(inputString) == 1 and inputString.isnumeric():
         return int(inputString)
+    else:
+        total = 0
+        stringNumbers = inputString.split(",")
+        for number in stringNumbers:
+            total += int(number)
+        return total    
+
 
 test()
