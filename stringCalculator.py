2612e7ef8cf1d721e9591ccfae42731d305a048c
siva-rama-pandiyan <sivaamc15@gmail.com>
2022-03-25 02:18:35 +0530

    String number with one number

--- stringCalculator.py
+++ stringCalculator.py
@@ -2,10 +2,18 @@
 def test():    
     #Empty string
     assert(add("")== 0)
+
+    #one Number
+    assert(add("4") == 4)
+    assert(add("2") == 2)
+
     print("All Test cases Completed Successfully")
-    
+
 #Main function
 def add(inputString):
     if len(inputString) == 0:
         return 0
+    elif len(inputString) == 1 and inputString.isnumeric():
+        return int(inputString)
+
 test()
